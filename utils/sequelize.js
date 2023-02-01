7f20e9c3a815f2a1f540d8f5ea193470d7973a6c
zeyad rashed <brwnboy2002@gmail.com>
2023-02-01 15:20:18 -0500

    sql

--- utils/sequelize.js
+++ utils/sequelize.js
@@ -1,14 +1,22 @@
 const Sequelize = require("sequelize");
+// const { db, db_user, db_pass, db_port } = require('config.json');
 
-const sequelize = new Sequelize(
-  process.env.DB,
-  process.env.DB_USERNAME,
-  process.env.DB_PASSWORD,
-  {
-    host: "db",
-    port: process.env.MYSQL_LOCAL_PORT,
-    dialect: "mysql",
-  }
-);
+// const sequelize = new Sequelize(
+//   // db,
+//   // db_user,
+//   // db_pass,
+//   process.env.DB,
+//   process.env.DB_USERNAME,
+//   process.env.DB_PASSWORD,
+//   {
+//     host: process.env.DB_HOST,
+//     port: process.env.MYSQL_LOCAL_PORT,
+//     dialect: "mariadb",
+//   }
+// );
+
+// const sequelize = new Sequelize('postgres://root:pass@example.com:5432/dbname') // Example for postgres
+
+const sequelize = new Sequelize('sqlite::memory:') // Example for sqlite
 
 module.exports = sequelize;
